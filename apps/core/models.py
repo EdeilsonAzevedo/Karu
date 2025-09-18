31032afe42267bd3568ad04d7d1d0092be4c5cc9
edeilsonazevedo <edeilsonazavedo22@gmail.com>
2025-09-18 15:46:56 -0300

    feat: adiciona app core e basemodel

---
+++ apps/core/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+import uuid
+
+
+class BaseModel(models.Model):
+    """
+    Modelo base para todas as tabelas do sistema.
+    Define campos comuns e comportamentos padrão.
+    """
+
+    id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False
+    )
+    created_at = models.DataTimeField(
+        auto_now_add = True, help_text="Data/hora em que o registro foi criado"
+    )
+
+    updated_at = models.DataTimeField(
+        auto_now_add = True, help_text="Data/hora da ultima atualização"
+    )
+    is_activate = models.BooleanField(
+        default = True, help_text="Indica se o registro está ativo"
+    )
+
+    class Meta:
+        abstract = True
+        ordering = ["-created_at"]
+
