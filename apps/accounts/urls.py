d9bcb8e96391ec9d06a582e537c4f88f0c524c2d
Filipe <filipeflicks@gmail.com>
2025-09-26 13:30:33 -0300

    feat: added authentication and simple login page

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,10 +1,20 @@
 from django.urls import path
 
-from . import views
+from .views import (
+    LogoutView,
+    MyLoginView,
+    dashboard_gestor,
+    dashboard_pais,
+    dashboard_profissional,
+    post_login_router,
+)
 
 app_name = "accounts"
-
 urlpatterns = [
-    path("login/", views.MyLoginView.as_view(), name="login"),
-    path("post-login/", views.post_login_router, name="post_login_router"),
+    path("login/", MyLoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path("post-login/", post_login_router, name="post_login_router"),
+    path("dashboard/gestor/", dashboard_gestor, name="dashboard_gestor"),
+    path("dashboard/profissional/", dashboard_profissional, name="dashboard_profissional"),
+    path("dashboard/pais/", dashboard_pais, name="dashboard_pais"),
 ]
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,12 +1,36 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
+
+from .forms import LoginForm
+from .models import User
 
 
 class MyLoginView(LoginView):
     template_name = "accounts/login.html"
+    authentication_form = LoginForm
 
 
 @login_required
 def post_login_router(request):
-    return redirect("admin:index")
+    u = request.user
+    if u.user_type == User.UserType.GESTOR:
+        return redirect("accounts:dashboard_gestor")
+    if u.user_type == User.UserType.PROFISSIONAL_SAUDE:
+        return redirect("accounts:dashboard_profissional")
+    return redirect("accounts:dashboard_pais")
+
+
+@login_required
+def dashboard_gestor(request):
+    return render(request, "accounts/dash_gestor.html")
+
+
+@login_required
+def dashboard_profissional(request):
+    return render(request, "accounts/dash_profissional.html")
+
+
+@login_required
+def dashboard_pais(request):
+    return render(request, "accounts/dash_pais.html")
