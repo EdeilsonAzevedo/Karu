d92a7690ce791942891891ac9ab99405d8c49f78
Edeilson Azevedo <107930230+EdeilsonAzevedo@users.noreply.github.com>
2025-10-10 09:00:42 -0300

    merge: pr #20 from feat/heroku-deployment (Feat/heroku deployment )
    * fix: corrigi redirecionamento ao clicar na logo  e atualiza menu de opções
    
    * chore: adiciona aviso de pagina em desenvolvimento
    
    * chore: atualiza estilo da pagina
    
    * chore:  atuliza path da rotasde login, logout e home
    
    * style: formata com make format (ruff)
    
    * chore: adiciona variáveis de ambiente ao .gitignore
    
    * chore: adiciona configuração  heroku
    
    * chore: define versão fixa do python para ser exercutada no heroku
    
    * chore: atuliza dependências para deploy
    
    * chore: configura para ler variaveis do.env
    
    * chore: coloca todas dependências do projeto em um .txt para deploy no heroku

--- config/settings.py
+++ config/settings.py
@@ -10,7 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
+
+# carregar .env
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-kax36l&)ke!dd2l1zm4z$l^2z)y3qj&-^ab*=%8a3b@12j+q0t"
+SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-dev-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
 
 # Application definition
@@ -52,6 +58,7 @@ LOGOUT_REDIRECT_URL = "/login/"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
