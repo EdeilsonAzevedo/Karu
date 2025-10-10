c47c480663bcefdcf79dfe939b0fe757802f413d
edeilsonazevedo <edeilsonazevedo22@gmail.com>
2025-10-10 15:13:04 -0300

    style: formata como make format (ruff)

--- apps/accounts/migrations/0001_initial.py
+++ apps/accounts/migrations/0001_initial.py
@@ -9,89 +9,242 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     initial = True
 
     dependencies = [
-        ('auth', '0012_alter_user_first_name_max_length'),
+        ("auth", "0012_alter_user_first_name_max_length"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='User',
+            name="User",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('password', models.CharField(max_length=128, verbose_name='password')),
-                ('last_login', models.DateTimeField(blank=True, null=True, verbose_name='last login')),
-                ('is_superuser', models.BooleanField(default=False, help_text='Designates that this user has all permissions without explicitly assigning them.', verbose_name='superuser status')),
-                ('username', models.CharField(error_messages={'unique': 'A user with that username already exists.'}, help_text='Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.', max_length=150, unique=True, validators=[django.contrib.auth.validators.UnicodeUsernameValidator()], verbose_name='username')),
-                ('first_name', models.CharField(blank=True, max_length=150, verbose_name='first name')),
-                ('last_name', models.CharField(blank=True, max_length=150, verbose_name='last name')),
-                ('email', models.EmailField(blank=True, max_length=254, verbose_name='email address')),
-                ('is_staff', models.BooleanField(default=False, help_text='Designates whether the user can log into this admin site.', verbose_name='staff status')),
-                ('is_active', models.BooleanField(default=True, help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.', verbose_name='active')),
-                ('date_joined', models.DateTimeField(default=django.utils.timezone.now, verbose_name='date joined')),
-                ('user_type', models.CharField(choices=[('gestor', 'Gestor'), ('profissional_saude', 'Profissional de Saúde'), ('pais', 'Pais/Responsáveis')], default='pais', max_length=32)),
-                ('groups', models.ManyToManyField(blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', related_name='user_set', related_query_name='user', to='auth.group', verbose_name='groups')),
-                ('user_permissions', models.ManyToManyField(blank=True, help_text='Specific permissions for this user.', related_name='user_set', related_query_name='user', to='auth.permission', verbose_name='user permissions')),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True, primary_key=True, serialize=False, verbose_name="ID"
+                    ),
+                ),
+                ("password", models.CharField(max_length=128, verbose_name="password")),
+                (
+                    "last_login",
+                    models.DateTimeField(blank=True, null=True, verbose_name="last login"),
+                ),
+                (
+                    "is_superuser",
+                    models.BooleanField(
+                        default=False,
+                        help_text="Designates that this user has all permissions without explicitly assigning them.",
+                        verbose_name="superuser status",
+                    ),
+                ),
+                (
+                    "username",
+                    models.CharField(
+                        error_messages={"unique": "A user with that username already exists."},
+                        help_text="Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.",
+                        max_length=150,
+                        unique=True,
+                        validators=[django.contrib.auth.validators.UnicodeUsernameValidator()],
+                        verbose_name="username",
+                    ),
+                ),
+                (
+                    "first_name",
+                    models.CharField(blank=True, max_length=150, verbose_name="first name"),
+                ),
+                (
+                    "last_name",
+                    models.CharField(blank=True, max_length=150, verbose_name="last name"),
+                ),
+                (
+                    "email",
+                    models.EmailField(blank=True, max_length=254, verbose_name="email address"),
+                ),
+                (
+                    "is_staff",
+                    models.BooleanField(
+                        default=False,
+                        help_text="Designates whether the user can log into this admin site.",
+                        verbose_name="staff status",
+                    ),
+                ),
+                (
+                    "is_active",
+                    models.BooleanField(
+                        default=True,
+                        help_text="Designates whether this user should be treated as active. Unselect this instead of deleting accounts.",
+                        verbose_name="active",
+                    ),
+                ),
+                (
+                    "date_joined",
+                    models.DateTimeField(
+                        default=django.utils.timezone.now, verbose_name="date joined"
+                    ),
+                ),
+                (
+                    "user_type",
+                    models.CharField(
+                        choices=[
+                            ("gestor", "Gestor"),
+                            ("profissional_saude", "Profissional de Saúde"),
+                            ("pais", "Pais/Responsáveis"),
+                        ],
+                        default="pais",
+                        max_length=32,
+                    ),
+                ),
+                (
+                    "groups",
+                    models.ManyToManyField(
+                        blank=True,
+                        help_text="The groups this user belongs to. A user will get all permissions granted to each of their groups.",
+                        related_name="user_set",
+                        related_query_name="user",
+                        to="auth.group",
+                        verbose_name="groups",
+                    ),
+                ),
+                (
+                    "user_permissions",
+                    models.ManyToManyField(
+                        blank=True,
+                        help_text="Specific permissions for this user.",
+                        related_name="user_set",
+                        related_query_name="user",
+                        to="auth.permission",
+                        verbose_name="user permissions",
+                    ),
+                ),
             ],
             options={
-                'verbose_name': 'user',
-                'verbose_name_plural': 'users',
-                'abstract': False,
+                "verbose_name": "user",
+                "verbose_name_plural": "users",
+                "abstract": False,
             },
             managers=[
-                ('objects', django.contrib.auth.models.UserManager()),
+                ("objects", django.contrib.auth.models.UserManager()),
             ],
         ),
         migrations.CreateModel(
-            name='GestorProfile',
+            name="GestorProfile",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('cpf', models.CharField(max_length=11, unique=True)),
-                ('telefone', models.CharField(blank=True, max_length=20)),
-                ('unidade', models.CharField(max_length=120)),
-                ('cargo', models.CharField(blank=True, max_length=100)),
-                ('departamento', models.CharField(blank=True, max_length=100)),
-                ('created_at', models.DateTimeField(auto_now_add=True)),
-                ('user', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, related_name='gestor', to=settings.AUTH_USER_MODEL)),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True, primary_key=True, serialize=False, verbose_name="ID"
+                    ),
+                ),
+                ("cpf", models.CharField(max_length=11, unique=True)),
+                ("telefone", models.CharField(blank=True, max_length=20)),
+                ("unidade", models.CharField(max_length=120)),
+                ("cargo", models.CharField(blank=True, max_length=100)),
+                ("departamento", models.CharField(blank=True, max_length=100)),
+                ("created_at", models.DateTimeField(auto_now_add=True)),
+                (
+                    "user",
+                    models.OneToOneField(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="gestor",
+                        to=settings.AUTH_USER_MODEL,
+                    ),
+                ),
             ],
         ),
         migrations.CreateModel(
-            name='PaisProfile',
+            name="PaisProfile",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('cpf', models.CharField(max_length=11, unique=True)),
-                ('telefone', models.CharField(blank=True, max_length=20)),
-                ('created_at', models.DateTimeField(auto_now_add=True)),
-                ('user', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, related_name='pais', to=settings.AUTH_USER_MODEL)),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True, primary_key=True, serialize=False, verbose_name="ID"
+                    ),
+                ),
+                ("cpf", models.CharField(max_length=11, unique=True)),
+                ("telefone", models.CharField(blank=True, max_length=20)),
+                ("created_at", models.DateTimeField(auto_now_add=True)),
+                (
+                    "user",
+                    models.OneToOneField(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="pais",
+                        to=settings.AUTH_USER_MODEL,
+                    ),
+                ),
             ],
         ),
         migrations.CreateModel(
-            name='Filho',
+            name="Filho",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('nome', models.CharField(max_length=100)),
-                ('data_nascimento', models.DateField()),
-                ('pais', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='filhos', to='accounts.paisprofile')),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True, primary_key=True, serialize=False, verbose_name="ID"
+                    ),
+                ),
+                ("nome", models.CharField(max_length=100)),
+                ("data_nascimento", models.DateField()),
+                (
+                    "pais",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="filhos",
+                        to="accounts.paisprofile",
+                    ),
+                ),
             ],
         ),
         migrations.CreateModel(
-            name='ProfissionalSaudeProfile',
+            name="ProfissionalSaudeProfile",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('cpf', models.CharField(max_length=11, unique=True)),
-                ('categoria', models.CharField(choices=[('Médico', 'Médico'), ('Enfermeiro', 'Enfermeiro'), ('Fonoaudiólogo', 'Fonoaudiólogo'), ('Fisioterapeuta', 'Fisioterapeuta'), ('Nutricionista', 'Nutricionista'), ('Psicólogo', 'Psicólogo'), ('Outro', 'Outro')], max_length=20)),
-                ('especialidade', models.CharField(blank=True, max_length=120)),
-                ('conselho', models.CharField(blank=True, max_length=20, null=True)),
-                ('numero_registro', models.CharField(blank=True, max_length=30, null=True)),
-                ('unidade', models.CharField(max_length=120)),
-                ('telefone', models.CharField(blank=True, max_length=20)),
-                ('created_at', models.DateTimeField(auto_now_add=True)),
-                ('user', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, related_name='profissional', to=settings.AUTH_USER_MODEL)),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True, primary_key=True, serialize=False, verbose_name="ID"
+                    ),
+                ),
+                ("cpf", models.CharField(max_length=11, unique=True)),
+                (
+                    "categoria",
+                    models.CharField(
+                        choices=[
+                            ("Médico", "Médico"),
+                            ("Enfermeiro", "Enfermeiro"),
+                            ("Fonoaudiólogo", "Fonoaudiólogo"),
+                            ("Fisioterapeuta", "Fisioterapeuta"),
+                            ("Nutricionista", "Nutricionista"),
+                            ("Psicólogo", "Psicólogo"),
+                            ("Outro", "Outro"),
+                        ],
+                        max_length=20,
+                    ),
+                ),
+                ("especialidade", models.CharField(blank=True, max_length=120)),
+                ("conselho", models.CharField(blank=True, max_length=20, null=True)),
+                ("numero_registro", models.CharField(blank=True, max_length=30, null=True)),
+                ("unidade", models.CharField(max_length=120)),
+                ("telefone", models.CharField(blank=True, max_length=20)),
+                ("created_at", models.DateTimeField(auto_now_add=True)),
+                (
+                    "user",
+                    models.OneToOneField(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="profissional",
+                        to=settings.AUTH_USER_MODEL,
+                    ),
+                ),
             ],
             options={
-                'constraints': [models.UniqueConstraint(condition=models.Q(('conselho__isnull', False), ('numero_registro__isnull', False)), fields=('conselho', 'numero_registro'), name='unique_conselho_registro_when_present')],
+                "constraints": [
+                    models.UniqueConstraint(
+                        condition=models.Q(
+                            ("conselho__isnull", False), ("numero_registro__isnull", False)
+                        ),
+                        fields=("conselho", "numero_registro"),
+                        name="unique_conselho_registro_when_present",
+                    )
+                ],
             },
         ),
     ]
