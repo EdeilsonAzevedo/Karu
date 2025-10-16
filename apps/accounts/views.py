99a2508e9bbdf964a937e7d3ab6c55c3d2396a79
Filipe <filipeflicks@gmail.com>
2025-10-16 14:46:37 -0300

    feat: added views and the url to reset password

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -9,8 +9,9 @@ from django.db.models import Count, Q
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.template.loader import render_to_string
+from django.views.decorators.http import require_http_methods
 
-from .forms import GestorSignupForm, PaisSignupForm, ProfissionalSignupForm
+from .forms import GestorSignupForm, PaisSignupForm, PasswordResetByDataForm, ProfissionalSignupForm
 
 User = get_user_model()
 
