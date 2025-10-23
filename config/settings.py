e0577fb3a08b5aeacb1a63b88655617e4793070e
MasioCesar <masiomc@gmail.com>
2025-10-23 12:15:28 -0300

    feat: mapas por micro e macroregioes

--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,7 @@ IS_CI = os.getenv("CI", "false").lower() == "true"
 SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-dev-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "False") == "True"
+DEBUG = True
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
